0ec665388652317cf4af7f10be928a4856a34661
Seydou Hanne <75482672+seydouhanne@users.noreply.github.com>
2026-04-12 19:07:53 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Me&U — Config
+// Garde ce fichier en sécurité (repo privé recommandé)
+window.CFG = {
+  token: "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",
+  owner: "seydouhanne",
+  repo:  "meu-app",
+  users: {
+    user1: { name: "Seydoux", emoji: "🌸", hash: "a3cb61aad8ae0c27ada5889dd7f2af97d7c774f42d666893d5a9f2139688f05a" },
+    user2: { name: "antiss", emoji: "💛", hash: "a3cb61aad8ae0c27ada5889dd7f2af97d7c774f42d666893d5a9f2139688f05a" }
+  }
+};
