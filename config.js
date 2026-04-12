0387a9efe25eb88be768438e14573e01f0d7fcaa
Seydou Hanne <75482672+seydouhanne@users.noreply.github.com>
2026-04-12 19:40:27 +0000

    Update  in config.js
    Updated  in configuration for security reasons.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Me&U — Config
 // Garde ce fichier en sécurité (repo privé recommandé)
 window.CFG = {
-  token: "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",
+  token: "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",
   owner: "seydouhanne",
   repo:  "meu-app",
   users: {
